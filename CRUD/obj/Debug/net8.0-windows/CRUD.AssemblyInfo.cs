988c39a45d7e764142fd19436eba504ccb35db82
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2025-02-17 14:07:29 -0300

    up

--- CRUD/obj/Debug/net8.0-windows/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0-windows/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4a6d13081ac5700ad436a157969262dbcd0928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d05cd899ffe47761d46fb0d028ac759284606bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
